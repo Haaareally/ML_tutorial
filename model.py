1a6b4584420d6521bde6c014ab0d5c4d8a4b3400
Harrily <147380095+Haaareally@users.noreply.github.com>
2025-11-22 15:31:49 +0800

    some changes

---
+++ model.py
@@ -0,0 +1,18 @@
+import pandas as pd 
+import numpy as np
+import sklearn
+import matplotlib.pyplot as plt
+
+# Basic operations to the data file 
+
+
+# Build our model 
+
+
+# Optimize our model 
+
+
+# Show the performance and evaluate
+
+
+# Save some figures or tables 
